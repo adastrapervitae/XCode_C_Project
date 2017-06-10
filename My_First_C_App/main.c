fec1778610fe0d6e5e855f4e7a428447c7dd6e01
AdAstraPerVitae <adastrapervitae@outlook.com>
2017-06-10 10:51:05 +0300

    Initial Commit

---
+++ My_First_C_App/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  My_First_C_App
+//
+//  Created by Roman Romanov on 10.06.17.
+//  Copyright © 2017 Drew My Screen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
